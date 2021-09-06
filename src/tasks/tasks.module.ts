e7b1985a913f112035bf1630611f604ed2104ed1
BernardoMB <bmondragonbrozon@gmail.com>
2021-09-06 15:04:20 -0500

    Added tasks repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TasksRepository } from './trask.repository';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([TasksRepository])
+  ],
   controllers: [TasksController],
   providers: [TasksService],
   exports: [],
